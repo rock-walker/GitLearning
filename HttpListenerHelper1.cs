e2a996de3e8a5e1579c386615d36eec9951ad640
Andrew Melnikov <melnikov.work@gmail.com>
2015-02-08 18:53:18 +0300

    dor amensssdfsing1

--- HttpListenerHelper1.cs
+++ HttpListenerHelper1.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Syst
 
 namespace ConsoleApplication4
 {
 	class HttpListenerHelper1
 	{
+		private int amendIt;
 	}
 }
