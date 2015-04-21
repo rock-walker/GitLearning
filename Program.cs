18c05f4c94b25e7fd79ba2ef3348af6b1d82e7d9
Andrew Melnikov <melnikov.work@gmail.com>
2015-04-21 23:15:04 +0300

    thread with static

--- Program.cs
+++ Program.cs
@@ -5,50 +5,27 @@ using System.Threading;
 
 namespace ConsoleApplication4
 {
-	class Program
+	class ThreadTest
 	{
+		private static bool done;
+		private static object locker = new object();
+
 		static void Main()
 		{
-			
-			var hl = new HttpListener();
-			hl.Start();
+			Thread.CurrentThread.Name = "main";
+			var t = new Thread(Go);
+			t.Name = "worker";
+			t.Start();
 
-			var res = hl.BeginGetContext(StartConnection, hl);
-			res.AsyncWaitHandle.WaitOne();
+			Go();
 		}
 
-		public static void StartConnection(IAsyncResult res)
+		public static void Go()
 		{
-			
-			 var listener = (HttpListener)res.AsyncState;
-			 HttpListenerContext context = listener.EndGetContext(res);
-			 // Obtain a response object. add comment
-			//try to amend
-			 HttpListenerResponse response = context.Response;
-			 // Construct a response. 
-			 string responseString = "<HTML><BODY> Learning GIT! " + DateTime.Now + "</BODY></HTML>";
-			 byte[] buffer = Encoding.UTF8.GetBytes(responseString);
-			 // Get a response stream and write the response to it.
-			 response.ContentLength64 = buffer.Length;
-			 System.IO.Stream output = response.OutputStream;
-			 output.Write(buffer, 0, buffer.Length);
-			 // You must close the output stream.
-			 Thread.Sleep(4000);
-			 output.Close();
+			for (int i = 0; i < 10; i++)
+			{
+				Console.Write(Thread.CurrentThread.Name);
+			}
 		}
-
-		public static void EndConnection(IAsyncResult res)
-		{
-			if (res.IsCompleted)
-				Console.WriteLine("cool");
-		}
-
-		public void Gotomaster()
-		{
-			var list = new HttpListener();
-			list.Start();
-		}
-
-
 	}
 }
